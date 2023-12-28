e0204210ccec7b62c569de540adba49205ab0b56
Satwik Agrawal <satwikcoolagrawal@gmail.com>
2023-12-28 13:07:17 +0530

    menu bar hidden

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 const {app, BrowserWindow,session}=require('electron');
 const url  = require('url');
 const path =require('path');
-
+const dotenv = require('dotenv');
+dotenv.config();
+const isDev=process.env.NODE_ENV ==='DEV'
 
 // const { session } = require('electron')
 
-
+console.log(isDev);
 
 function createMainWindow(){
     const mainWindow= new BrowserWindow({
-        titleBarStyle:"hidden",
+        // titleBarStyle:"hidden",
+        autoHideMenuBar: true,
         title :"Entry Managment App",
         width:1000,
         height:600,
         minWidth:500,
         minHeight:300,
         webPreferences:{
-            // devTools:false,
+            devTools:isDev,
             nodeIntegration:true,
             contextIsolation:true,
             preload:path.join(__dirname,'preload.js')
         }
     });
-    mainWindow.webContents.openDevTools()
+    if (isDev) mainWindow.webContents.openDevTools()
 
     const startUrl=url.format({
         pathname: path.join(__dirname,'./app/build/index.html'),
     });
-
-
     
     mainWindow.loadURL(startUrl);
 }
