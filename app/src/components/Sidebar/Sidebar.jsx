7f929df7626e7754fef6e6b9253fd8e67e1b9fbb
Saksham Verma <sakshamverma112233@gmail.com>
2023-12-29 22:00:12 +0530

    TitleBarNSideBarNOFUNCTION

--- app/src/components/Sidebar/Sidebar.jsx
+++ app/src/components/Sidebar/Sidebar.jsx
@@ -20,7 +20,7 @@ function Sidebar() {
       <div
         className={` ${
           open ? "w-72" : "w-20 "
-        } bg-dark-purple h-screen p-5  pt-8 relative duration-300`}
+        } bg-gray-400 h-screen p-5  pt-8 relative duration-300`}
       >
         <img
           src={require("./assets/control.png")}
@@ -30,7 +30,7 @@ function Sidebar() {
         />
         <div className="flex gap-x-4 items-center">
           <img
-            src={require("./assets/control.png")}
+            src={require("./assets/logo.png")}
             className={`cursor-pointer duration-500 ${
               open && "rotate-[360deg]"
             }`} alt="1"
